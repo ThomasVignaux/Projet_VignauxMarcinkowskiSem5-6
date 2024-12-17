04162e77a40cf601d5be78056a241b997a4fef80
LucasM <77438014+L1nkFR@users.noreply.github.com>
2024-12-17 18:11:13 +0100

    modification structure projet

--- models/Element.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const elementSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  description: { type: String },
-  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
-});
-
-module.exports = mongoose.model('Element', elementSchema);
